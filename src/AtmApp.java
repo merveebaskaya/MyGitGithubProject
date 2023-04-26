b7dcade79e0789bbf021d9421ac389cd445a11c9
merveebaskaya <merveeuludag@gmail.com>
2023-04-26 12:02:12 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,7 +1,9 @@
 public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir");
-        //versiyon1gi
+        //versiyon1
+        System.out.println("Java is easy!");
+        //version2
 
 
 
